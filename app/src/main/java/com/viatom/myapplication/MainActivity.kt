0e0a5b515e423d6bc58bb76dbed95169b21d9f8f
lgh0322 <liguanghao@viatomtech.com>
2021-09-24 17:24:14 +0800

    fdgd

--- app/src/main/java/com/viatom/myapplication/MainActivity.kt
+++ app/src/main/java/com/viatom/myapplication/MainActivity.kt
@@ -2,12 +2,58 @@ package com.viatom.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.viatom.myapplication.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 
 class MainActivity : AppCompatActivity() {
+    // 定义接收网络数据的字节数组
+    var inBuff = ByteArray(560)
+
+    // 以指定的字节数组创建准备接收数据的DatagramPacket对象
+    private val inPacket = DatagramPacket(inBuff, inBuff.size)
+
+    // 定义一个用于发送的DatagramPacket对象
+    private var outPacket: DatagramPacket? = null
+
+    lateinit var datagramSocket: DatagramSocket
+    val dataScope = CoroutineScope(Dispatchers.IO)
+
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Thread{UdpClient.main()}.start()
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        dataScope.launch {
+            DatagramSocket().use {
+                outPacket = DatagramPacket(
+                    ByteArray(0), 0, InetAddress.getByName("192.168.5.108"),1497)
+                it.send(outPacket)
+                val gg=it.receive(inPacket)
+                val xx=inPacket.data
+
+                Log.e("fuck",xx.size.toString())
+            }
+
+
+
+        }
 
     }
+    fun mainX(byteArray: ByteArray):String {
+        var fuc=""
+        for (b in byteArray) {
+            val st = String.format("%02X", b)
+            fuc+=("$st  ");
+        }
+        return fuc
+    }
+
 }
